7d38a1d2adb233df4c5cbce40848dddbda078e1a
Randy Steele <steelerx155@gmail.com>
2023-10-16 04:33:00 -0400

    added Tweets model and controller and set the root path

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
 
 
   devise_for :users
-  root to: 'home#index'
+  resources: tweets, except: [:edit, :update]
+  root to: 'tweets#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
