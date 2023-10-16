2b5922981bee3b5769c8dce9806e9a3b9f3796d3
Randy Steele <steelerx155@gmail.com>
2023-10-16 04:48:47 -0400

    updated create and index action in Tweets controller and built the Tweets index view

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     authenticate :user, lambda { |u| u.admin? } do
       mount Sidekiq::Web => '/sidekiq'
     end
-
+  end
 
   devise_for :users
   resources: tweets, except: [:edit, :update]
